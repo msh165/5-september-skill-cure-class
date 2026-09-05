7e86a54e0f661bc00e8d2179a1c5a4b4f2e378fd
Sehal Hasan <sehal.hasan16@gmail.com>
2026-09-05 15:46:00 +0530

    Connected env to my main

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from important_functions import *
 
 print(f"Your answer is {adding_2_numbers(2,3)}")
+
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env file
+load_dotenv()
+
+# Access them using os.getenv
+secret_key = os.getenv("api_key")
+print(secret_key)
